e0f6afd88685d94008a286b8566624081b8a2ea0
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-08 18:57:03 -0300

    genéricos

--- Project 1/newbank/app/ts/views/View.ts
+++ Project 1/newbank/app/ts/views/View.ts
@@ -1,10 +1,19 @@
-class View {
+class View<T> {
 
     protected _elemento: Element;
 
     constructor(selector: string) {
 
         this._elemento = document.querySelector(selector);
+    }
+
+    update(model: T) {
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model: T): string {
+
+        throw new Error('Você deve implementar o método template');
     }
 
 }
