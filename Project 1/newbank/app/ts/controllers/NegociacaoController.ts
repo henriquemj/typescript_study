4831bc2f17912762c347f94feef727000c1597a4
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-07 22:37:08 -0300

    Negociacoes.TS - array

--- Project 1/newbank/app/ts/controllers/NegociacaoController.ts
+++ Project 1/newbank/app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes();
 
     constructor() {
 
@@ -20,8 +21,8 @@ class NegociacaoController {
             parseInt(this._inputQuantidade. value),
             parseFloat(this._inputValor.value)
         );
-
-        console.log(negociacao);
+        
+        this._negociacoes.adiciona(negociacao);
     }
 }
 
