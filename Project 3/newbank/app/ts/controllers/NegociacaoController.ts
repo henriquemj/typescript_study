7f5dd488cc9663ebdeb67df97af4792c13d0357b
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-12 21:56:23 -0300

    NegociacaoService

--- Project 3/newbank/app/ts/controllers/NegociacaoController.ts
+++ Project 3/newbank/app/ts/controllers/NegociacaoController.ts
@@ -1,84 +1,91 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacao, Negociacoes } from '../models/index';
-import { domInject } from '../helpers/decorators/index';
+import { domInject, throttle } from '../helpers/decorators/index';
+import { NegociacaoParcial } from '../models/index';
+import { NegociacaoService } from './../services/index';
 
 export class NegociacaoController {
-
+    
     @domInject('#data')
     private _inputData: JQuery;
-
+    
     @domInject('#quantidade')
     private _inputQuantidade: JQuery;
-
+    
     @domInject('#valor')
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
+
+    private _service = new NegociacaoService();
     
     constructor() {
         this._negociacoesView.update(this._negociacoes);
     }
-
-    adiciona(event: Event) {
-        event.preventDefault();
-
+    
+    @throttle()
+    adiciona() {
+        
         let data = new Date(this._inputData.val().replace(/-/g, ','));
-
+        
         if(!this._ehDiaUtil(data)) {
-
+            
             this._mensagemView.update('Somente negociações em dias úteis, por favor!');
             return 
         }
-
+        
         const negociacao = new Negociacao(
             data, 
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
-        );
-
-        this._negociacoes.adiciona(negociacao);
-
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagemView.update('Negociação adicionada com sucesso!');
-
-    }
-
-    private _ehDiaUtil(data: Date) {
-
-        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
-    }
-
-    importaDados() {
-
-        function isOk(res: Response) {
-            if(res.ok) {
-                return res;
-            } else {
-                throw new Error(res.statusText);
-            }
+            );
+            
+            this._negociacoes.adiciona(negociacao);
+            
+            this._negociacoesView.update(this._negociacoes);
+            this._mensagemView.update('Negociação adicionada com sucesso!');
+            
         }
         
-        fetch('http://localhost:8080/dados')
-        .then(res => isOk(res))
-        .then(res => res.json())
-        .then((dados: any[]) => {
-            dados
-                .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
-                .forEach(negociacao => this._negociacoes.adiciona(negociacao))
-            this._negociacoesView.update(this._negociacoes);
-        })
-        .catch(err => console.log(err));
+        private _ehDiaUtil(data: Date) {
+            
+            return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+        }
+        
+        @throttle()
+        importaDados() {
+            
+            function isOk(res: Response) {
+
+                if(res.ok) {
+                    return res;
+                } else {
+                    throw new Error(res.statusText);
+                }
+            }
+            
+                this._service
+                    .obterNegociacoes(isOk)
+                    .then(negociacoes => {
+                        
+
+                        negociacoes.forEach(negociacao => 
+                            this._negociacoes.adiciona(negociacao));
+                            
+                           this._negociacoesView.update(this._negociacoes);
+                        });
+                        
+        }
     }
-}
-
-enum DiaDaSemana {
-
-    Domingo, 
-    Segunda, 
-    Terca, 
-    Quarta, 
-    Quinta, 
-    Sexta, 
-    Sabado
-}
+    
+    enum DiaDaSemana {
+        
+        Domingo, 
+        Segunda, 
+        Terca, 
+        Quarta, 
+        Quinta, 
+        Sexta, 
+        Sabado
+    }
